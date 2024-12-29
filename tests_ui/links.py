75fcc82d110efd47d531fa42befd1c1c5cd2af6c
lambotik <lambotik@users.noreply.github.com>
2024-12-29 01:10:53 +0000

    Automated deployment: Sun Dec 29 01:10:53 UTC 2024 1e0f9c84ef680d9f1938294d6e8bb16c73db7f02

--- tests_ui/links.py
+++
@@ -1,4 +0,0 @@
-class TestDataLinks:
-    main_page = 'https://oblache.areso.pro/'
-    register_page = 'https://oblache.areso.pro/register.html'
-    profile_page = 'https://oblache.areso.pro/profile.html'
